48b68023a7bf2502b4269a5023577f93567adf19
xpriidex <lopezarcefelipe@gmail.com>
2017-04-26 11:59:58 -0300

    segundo commit

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,13 +2,14 @@
  * Created by Felipe on 4/26/2017.
  */
 public class Game {
-
+    private int total=0;
     public void roll(int i) {
+        total=total+i;
 
 
     }
 
     public int score() {
-        return 8;
+        return total;
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,17 +7,36 @@ import static org.junit.Assert.assertEquals;
  * Created by Felipe on 4/26/2017.
  */
 public class GameTest {
+    Game game;
+    int result;
+    @Before
+    public void setUp() throws Exception {
+        //Arrange
+        game = new Game();
+
+    }
+
     @Test
     public void roll3AndRoll5Return8(){
-        //Arrange
-        Game game = new Game();
-        int result;
+
         //Act
         game.roll(3);
         game.roll(5);
         result=game.score();
         //Assert
         assertEquals(8,result);
+    }
+    @Test()
+    public void roll7AndRoll2Return7(){
+        //Act
+        game.roll(3);
+        game.roll(5);
+        game.roll(7);
+        game.roll(2);
+        result=game.score();
+        //Assert
+        assertEquals(17,result);
+
     }
 
 }
